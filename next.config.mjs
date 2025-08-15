6c0c1e021fca9e6aaf4b93f6d9f694e24cadea5e
GitHub User <user@example.com>
2025-08-15 15:46:56 +0900

    Setup for Vercel deployment with server actions

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  // GitHub Pages에서 리포지토리 이름이 mysc-training이 아니면 아래 basePath를 수정하세요
-  // basePath: '/mysc-training',
-  // assetPrefix: '/mysc-training',
   eslint: {
     ignoreDuringBuilds: true,
   },
